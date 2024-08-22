025c87c1165a781f29b888f8a575a9ffcfc7ef81
Sergei <srjchsv@gmail.com>
2024-08-22 19:58:22 +0000

    refactor: podcast transcription styles

--- frontend/src/components/Transcriptions.js
+++ frontend/src/components/Transcriptions.js
@@ -11,7 +11,25 @@ const Transcriptions = ({ transcriptions }) => {
         <h2 className={styles.title}>Transcriptions</h2>
         <div className={styles.transcriptions}>
         {transcriptions.map((text, index) => (
-            (text.includes("Host:")? <li key={index} className={styles.host}>{text}</li> : text.includes("USERS INTERACTION:") ? <li key={index} className={styles.usersInteraction}>{text}</li>: <li key={index} className={styles.guest}>{text}</li>) 
+            (text.includes("Host:") ? (
+                <li key={index} className={styles.host}>
+                    <strong className={styles.speaker}>Host:</strong>
+                    {text.split("Host:")[1]}
+                </li>
+            ) : text.includes("USERS INTERACTION:") ? (
+                <li key={index} className={styles.usersInteraction}>
+                    <strong className={styles.speaker}>You:</strong>
+                    {text.split("USERS INTERACTION:")[1]}
+                </li>
+            ) : (text.includes("Guest:") ? (
+                <li key={index} className={styles.guest}>
+                    <strong className={styles.speaker}>Guest:</strong>
+                    {text.split("Guest:")[1]}
+                </li>
+            ): (
+                <li key={index} className={styles.guest}>{text}</li>
+            )))
+            // (text.includes("Host:")? <li key={index} className={styles.host}>{text}</li> : text.includes("USERS INTERACTION:") ? <li key={index} className={styles.usersInteraction}>{text}</li>: <li key={index} className={styles.guest}>{text}</li>) 
         ))}
         </div>
        </>
